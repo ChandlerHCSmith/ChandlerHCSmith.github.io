490d204d4df8a7b8006fe9e437b5128f3964c7da
ChandlerHCSmith <chandlerhsmith@yahoo.com>
2014-05-11 09:34:08 -0700

    u1_w3_1_Git_Started

---
+++ unit1_projects/week_3/w3_1_git_started_my_solutions.js
@@ -0,0 +1,145 @@
+
+
+"Git" Started
+
+// Learning Competencies
+
+// Use git commands in the terminal
+// Fetch changes from a forked repository
+// Push and pull your own changes between your local and remote repositories
+
+----------Release 0: Install Git---------------------------------------
+//	Already had installed Git.  I was reading through some of the Michael Hartl book.  Haven't finished yet.
+
+
+----------Release 1: Research---------------------------------------
+//	Done for now.  Definitely could go deeper.
+
+
+----------Release 2: Basic Git Commands--------------------------------
+
+Define the following commands and describe how they work/what they do.
+
+//add
+//	Adds files to branches so they can be tracked and utilized.  Initially they are added to the staging area.
+
+//branch
+//	A copy of your code that you can "move along side", work on, and make separate committs..  Once done, your 
+// 		new code wasn't really useful and you reset or remove.  Or you can merge changes back into the master branch.	
+
+//checkout
+//	This is used to switch branches.
+
+//clone
+//	Creates an identical copy of a repository in a new respository.
+
+//commit
+//	Creates a history of changes.  This allows you to journey back in time.  Be sure to committ often.  
+//	Leave yourself lots of sign posts.
+//	 
+
+//fetch
+//	Graps materials from another repository.
+
+//log
+//	Shows a history of committs to help us make decisions and remember changes.
+
+//merge
+//	This takes changes that we have created on another branch and puts them into the original branch.
+
+//pull
+//	This takes other contributor's changes and we pull them into our original project.
+
+//push
+//	Tells Git where to put our committs
+//	git push -u origin master (origin is the remote and our local branch is named master)
+
+//	git push -u origin master
+
+//reset
+//	This allows us to unstage files.
+
+//rm
+//	Removes files from a given repository.  This will also stage the removal to make this thorough.
+
+//status
+//	Tells us where a file is located and when/what the last committ was.
+
+----------Release 3: Use Git-------------------------------------
+
+//	I practiced.
+
+
+----------Release 4: Git Workflow--------------------------------
+
+// Push files to a remote repository
+//		general form: git push [alias] [branch] 
+//		specific ex:  git push githubdevbootcamp phase_0_projects
+
+// Fetch changes
+//		general form: git fetch github
+//		specific ex:  git fetch githubdevbootcamp
+
+// Commit locally (stage first with git add)
+//		ex:  git commit -m 'Records player's moves'
+
+
+
+----------Release 5: Reflection----------------------------------
+
+// Reflection Guidelines
+// When writing your reflection, write it for an audience. Tell a story of what you tried and what you learned. It will make it easier for you to go back and re-teach yourself or know what you were thinking.
+
+// Remember, your reflections aren't just for teachers, they are for your fellow cohort mates as well, so be generous! You could end up helping a fellow student learn something new.
+
+// Answer the following questions (in addition to the challenge-specific questions) in your reflection:
+
+//1) What parts of your strategy worked? What problems did you face?
+//	I have the need to practice.  I am still not satisfied with my understanding of GitHub Workflow.
+//   I feel that I understand, for the most part, the overall flow.  I need to pratice, practice, practice the terminology and techniques.
+	// -	I create some work.
+	// -	I create a respository for this work.
+	// - 	I place my work in this repository.
+	// -	I commit and then synch/push.
+	// -	I do work and commit (This keeps a history.)
+	// -	Others can join.
+	// - 	Others can add work.
+	// -	Others create a branch of their own to work on.
+	// -	Commits are made frequently to leave time markers.
+	// -	We can put all of our work together.
+	// - 	I will call a friend of mine who I know can explain this and clear things up.
+
+//2) What questions did you have while coding? What resources did you find to help you answer them?
+
+//	Here are some resources that I utilized:
+
+// https://try.github.io
+// https://github.com/
+// http://gitref.org/basic/
+
+
+//3) What concepts are you having trouble with, or did you just figure something out? If so, what?
+		// -	Again, I think the overall idea at this level makes sense.
+		// -	GitHub allows several versions of the same project to be worked on.
+		// -	GitHub allows us to keep track of these different pieces.
+		// - 	GitHub allows us to delete pieces, add pieces/sources, merge.
+		// *	I really think that working with GitHub will make the process clear.
+
+//4) Did you learn any new skills or tricks?
+		// I know some of the terms and commands, but not fluently.
+
+//5) How confident are you with each of the Learning Competencies?
+		// I need repetition/practice.
+
+//6) Which parts of the challenge did you enjoy?
+		// I find it rewarding to understand systems, especially one's I need to know.  I like very much the idea
+		//		that versions of past work have been saved.
+		// I like very much that colaboration at this level is already here.
+
+//7) Which parts of the challenge did you find tedious?
+		// I just need to practice the commands.  I enjoyed the tasks.
+
+
+
+
+
