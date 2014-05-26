edb8f861b54fe88f6a60ed0b2b253296f7416c99
ChandlerHCSmith <chandlerhsmith@yahoo.com>
2014-05-25 19:26:55 -0700

    u2_w5_10_BONUS_rpn_thought_Optional

---
+++ unit2_projects/week_5/u2_w5_10_BONUS-rpn.rb
@@ -0,0 +1,18 @@
+
+
+#  Reverse Polish Notation Calculator
+
+#  	I started with this challenge first, since it appears at the top of the list.
+# 	I read it over and went off to work thinking about RPN.  This was Monday, Day 1 of Week 5.
+
+#  	When I came home I read the 1_die challenge and saw the solution quickly, so I worked the 1_die challenge out.
+# 	This took me in the direction of working on challenge 2 and then 3 and so on.  I had more family committments this
+# 	week than normal and by Friday I noticed the word BONUS and thought of it as an Optional assignment as DBC sometimes
+# 	has.
+
+# 	I wanted to work out the answer and still will next week if this is okay.
+
+#  	I only became aware that this must not be optional when my submission page would not advance.
+
+
+
